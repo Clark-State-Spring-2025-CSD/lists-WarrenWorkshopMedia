36995c1815b726a581ff12098650f5af937e89c0
Alex Warren <WarrenWorkshopMedia@gmail.com>
2025-03-05 13:15:27 -0500

    Update meanmedian.py

--- meanmedian.py
+++ meanmedian.py
@@ -3,3 +3,35 @@
 #Calculate and display the mean and median
 #This is a guided practice. You can follow the video or your instructor will go
 #over this in class
+
+
+TheList = []
+#TheList = [0,0,0,0,0]
+
+for i in range(5):
+    TheList.append(int(input(f"Number {i+1}: ")))
+    #TheList[i] = int(input(f"Numer{i+1}: "))
+
+print("Original order: " + str(TheList))
+
+TheList.sort()
+
+print(f"small to large: {TheList}")
+
+TheList.sort(reverse=True)
+
+print(f"large to small: {TheList}")
+
+median = TheList[2]
+
+sum = 0
+
+for i in range(5):
+    sum += TheList[i]
+
+#for value in TheList:
+    #sum += value
+
+average = sum / 5
+
+print(f"the medium is {median} and the mean is {average}")
